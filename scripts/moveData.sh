8dd396ea543a7c913861cb1d988a1d000f764eb6
Gaurav Aggrahari <gaurav.kumar.aggrahari@ibm.com>
2022-11-25 14:15:18 +0530

    Initial Shell Script to move data
    Signed-off-by: Gaurav Aggrahari <gaurav.kumar.aggrahari@ibm.com>

---
+++ scripts/moveData.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#We need srcMountPath, dstMountPath as inputs
+policyADate=$(echo $3 | cut -d ">" -f 2)
+#recursevily walk the filesystem
+find $1 -print0 | while IFS= read -r -d '' file;
+    do  echo "$file";
+    #Get the Access Time of the file
+    aDate=$(stat $file | grep -i Access | tail -1 | awk -F " " '{print $2}')
+    #Get the date in above format
+    currDate=$(date +%Y-%m-%d)
+    diff=$(( ($(date -d $aDate +%s) - $(date -d $currDate +%s)) / 86400))
+    if [ $diff>$policyADate ]
+    then
+        #Move the file to dst
+        mv $1/$file $2
+        #Make a symlink
+        ln -s $file
+    fi
+done;
